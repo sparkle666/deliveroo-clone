97319b0318f46e2e1fd0f1fed7ba14c5ebd093ec
Sixtus Anyanwu <sikky606@gmail.com>
2022-11-10 13:28:13 +0100

    added a home screen header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,11 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import {ChevronIcon} from 'react-native-heroicons'
+import {
+    ChevronDownIcon, UserIcon, 
+    MagnifyingGlassIcon, 
+    AdjustmentsVerticalIcon} from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -15,18 +18,33 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-        <View className = "flex-row space-x-1 ml-2">
-            <Image source = {{
-                uri: "https://links.papareact.com/wru"
-            }} 
-            className = "w-7 h-7 rounded-full bg-gray-400 p-4"
-            />
-            <View>
-                <Text className = "text-xd font-bold text-gray-400">Deliver now!</Text>
-                <Text className = "font-bold text-xl">Current Location
-                    <ChevronIcon />
-                </Text>
+        <View className = "bg-white">
+            <View className = "flex-row space-x-1 ml-2 items-center pt-5 px-2 mb-2">
+                <Image source = {{
+                    uri: "https://links.papareact.com/wru"
+                }} 
+                className = "w-7 h-7 rounded-full bg-gray-400 p-4"
+                />
+                <View className = "flex-grow">
+                    <Text className = "text-xs font-bold text-gray-400">Deliver now!</Text>
+                    <Text className = "font-bold text-xl">Current Location
+                        <ChevronDownIcon size = {16} color = '#00CCBB' className = "" />
+                    </Text>
+                </View>
+                <UserIcon size = {35} color = '#00CCBB' />
             </View>
+            {/* Input */}
+            <View className = "flex-row items-center px-3">
+                <View className = "flex-row flex-1 space-x-2 bg-gray-200 p-2 rounded">
+                    <MagnifyingGlassIcon  size = {20} color = 'gray' />
+                    <TextInput
+                    placeholder='Resturants and cuisines'
+                    keyboardType='default'
+                    />
+                </View>
+                <AdjustmentsVerticalIcon size = {24} color = '#00CCBB' />
+            </View>
+            <Text> Hello </Text>
         </View>
     </SafeAreaView>
   )
